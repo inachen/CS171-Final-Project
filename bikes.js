a775e5d9b45e6f8632525b663fc70c5be36efe71
ichen <weilinchen@college.harvard.edu>
2014-04-10 21:05:25 -0400

    added stations, redid layout

--- bikes.js
+++ bikes.js
@@ -1,51 +1,52 @@
 var margin = {
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 10
 };
 
 var view = {
-    w: 1060,
-    h: 800
+    w: 1160,
+    h: 700
 }
 
 var width = view.w - margin.left - margin.right;
 var height = view.h - margin.bottom - margin.top;
-var buttonh = 200;
+var map_width = 500;
 var centered;
 
 var graphVis = {
     x: 100,
     y: 10,
-    w: width,
-    h: height - buttonh
+    w: width - map_width,
+    h: height
 };
 
 var mapVis = {
     x: 10,
     y: 10,
-    w: width,
+    w: map_width,
     h: height 
 }
 
 var graphCanvas = d3.select("#graphVis").append("svg").attr({
-    width: width + margin.left + margin.right,
-    height: height - buttonh + margin.top + margin.bottom
+    width: graphVis.w +200+ margin.left + margin.right,
+    height: height + margin.top + margin.bottom
     })
 
 var mapCanvas = d3.select("#mapVis").append("svg").attr({
-    width: width,
-    height: height - buttonh
+    width: map_width + margin.left + margin.right,
+    height: height 
 })
 
 var graphsvg = graphCanvas.append("g").attr({
         transform: "translate(" + margin.left + "," + margin.top + ")"
     });
 
-var mapsvg = mapCanvas.append("g").attr({
-        transform: "translate(" + margin.left + "," + margin.top + ")"
-    });
+var mapsvg = mapCanvas.append("g")
+    // .attr({
+    //     transform: "translate(" + margin.left + "," + margin.top + ")"
+    // });
 
 // -----------------------------------
 // map vis
@@ -54,29 +55,73 @@ var mapsvg = mapCanvas.append("g").attr({
 var projection = d3.geo.mercator().scale(1).precision(.1);//.translate([width / 2, height / 2])
 var path = d3.geo.path().projection(projection);
 
-// var screencoord = projection([-71.074778, 42.349278]);
+var station_tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([0, 0])
+  .direction('e')
+  .html(function(d) {
+    return d['name'];
+  })
 
-// graphsvg.append("circle")
-//     .attr("cx", screencoord[0])
-//     .attr("cy", screencoord[1])
-//     .attr("r", 3)
+mapsvg.call(station_tip);
 
+function loadStations() {
+    d3.csv("/data/dc-stations.csv",function(error,data){
+        mapsvg.selectAll(".station")
+            .data(data)
+        .enter().append("circle")
+        .filter(function(d) { return (projection([+d["long"], +d["lat"]]) != null ) })  
+          .attr("class", "station hasData")
+          .attr("cx", function(d) { coords = projection([+d["long"], +d["lat"]]);
+                if (coords != null){return coords[0] }})
+         .attr("cy", function(d) { coords = projection([+d["long"], +d["lat"]]); 
+                if (coords != null){return coords[1] }})
+         .attr("r", 2)
+        .on('mouseover', station_tip.show)
+        .on('mouseout', station_tip.hide)
+    });
+}
+
+function clicked(d) {
+  var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 3;
+    centered = d;
+  } else {
+    x = mapVis.w / 2;
+    y = mapVis.h / 2;
+    k = 1;
+    centered = null;
+  }
+
+  mapsvg.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+  mapsvg.transition()
+      .duration(750)
+      .attr("transform", "translate(" + mapVis.w / 2 + "," + mapVis.h / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1/k + "px");
+}
 
 // boston
-d3.json("/data/boston.json", function(error, data) {
+// d3.json("/data/boston.json", function(error, data) {
 
-    // console.log(data);
-    projection.scale(100000).center([-71.090813, 42.309228]);
+//     // console.log(data);
+//     projection.scale(100000).center([-71.090813, 42.309228]);
 
-    var cityMap = topojson.feature(data,data.objects.boston).features;
-    // console.log(cityMap);
+//     var cityMap = topojson.feature(data,data.objects.boston).features;
+//     // console.log(cityMap);
 
-    mapsvg.selectAll(".city").data(cityMap).enter().append("path")
-        .attr("class", "city")
-        .attr("d", path).on("click", clicked);
-    // see also: http://bl.ocks.org/mbostock/4122298
+//     mapsvg.selectAll(".city").data(cityMap).enter().append("path")
+//         .attr("class", "city")
+//         .attr("d", path).on("click", clicked);
+//     // see also: http://bl.ocks.org/mbostock/4122298
 
-});
+// });
 
 // chicago
 // d3.json("/data/chicago.json", function(error, data) {
@@ -95,45 +140,22 @@ d3.json("/data/boston.json", function(error, data) {
 // });
 
 // washington
-// d3.json("/data/washington.json", function(error, data) {
+d3.json("/data/washington.json", function(error, data) {
 
-//     // console.log(data);
-//     projection.scale(100000).center([-77.00386, 38.894156]);
+    // console.log(data);
+    projection.scale(80000).center([-77.05386, 38.954156]).translate([mapVis.w/2, mapVis.h/2]);
 
-//     var cityMap = topojson.feature(data,data.objects.washington).features;
-//     // console.log(cityMap);
+    var cityMap = topojson.feature(data,data.objects.washington).features;
+    // console.log(cityMap);
 
-//     mapsvg.selectAll(".city").data(cityMap).enter().append("path")
-//         .attr("class", "city")
-//         .attr("d", path).on("click", clicked);
-//     // see also: http://bl.ocks.org/mbostock/4122298
+    mapsvg.selectAll(".city").data(cityMap).enter().append("path")
+        .attr("class", "city")
+        .attr("d", path).on("click", clicked);
 
-// });
+    loadStations();
 
-function clicked(d) {
-  var x, y, k;
+});
 
-  if (d && centered !== d) {
-    var centroid = path.centroid(d);
-    x = centroid[0];
-    y = centroid[1];
-    k = 3;
-    centered = d;
-  } else {
-    x = width / 2;
-    y = height / 2;
-    k = 1;
-    centered = null;
-  }
-
-  mapsvg.selectAll("path")
-      .classed("active", centered && function(d) { return d === centered; });
-
-  mapsvg.transition()
-      .duration(750)
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-      .style("stroke-width", 1/k + "px");
-}
 
 // console.log(screencoord[2])
 // Source links
@@ -142,6 +164,7 @@ function clicked(d) {
 // convert geojson to topojson: https://github.com/mbostock/topojson/wiki/Command-Line-Reference
 // chose mercator (good for cities)
 // finding long-lat: http://itouchmap.com/latlong.html
+// zooming map: http://bl.ocks.org/mbostock/4122298
 
 // -----------------------------------
 // graph vis
@@ -162,7 +185,7 @@ createGraph = function(param,type) {
         
         var yScaleGraph = d3.scale.linear()
                         .domain([points.min(),points.max()])
-                        .range([graphVis.y+graphVis.h-margin.top,graphVis.y+margin.top])
+                        .range([graphVis.y+graphVis.h-margin.top-300,graphVis.y+margin.top])
 
         console.log(objs)
 
